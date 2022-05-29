aaff46f34839e172cc979e55ff46e30c036b7f63
Giorgos Komninos <admin@gkomninos.com>
2022-05-29 13:25:10 +0300

    adds user struct and mem storage

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,43 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/go-chi/chi"
 )
+
+var ErrUserExists = errors.New("user already exists")
+
+type User struct {
+	Address string
+}
+
+type MemStorage struct {
+	lock  sync.Mutex
+	users map[string]User
+}
+
+func (m *MemStorage) CreateIfNotExists(u User) error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	if _, exists := m.users[u.Address]; exists {
+		return ErrUserExists
+	}
+	m.users[u.Address] = u
+	return nil
+}
+
+func NewMemStorage() *MemStorage {
+	ans := MemStorage{
+		users: make(map[string]User),
+	}
+	return &ans
+}
+
+// ============================================================================
 
 func RegisterHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
